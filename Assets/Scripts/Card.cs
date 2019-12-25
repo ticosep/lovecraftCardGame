7ee4c88bbeb261b9eea2266661008086f52588a4
Egisto Parronchi Neto <ticoparronchi@gmail.com>
2019-12-25 13:06:55 -0300

    Add card with assets

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="New Card", menuName="Card" )]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    public string description;
+
+    public int insanity;
+    public int life;
+    public int attack;
+
+    public Sprite artWork;
+
+}
---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour
+{
+    public Card card;
+
+    public Text cardName;
+    public Text description;
+
+    public Text attack;
+    public Text life;
+    public Text insanity;
+
+    public Image artWork;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cardName.text = card.cardName;
+        description.text = card.description;
+
+        life.text = card.life.ToString();
+        attack.text = card.attack.ToString();
+        insanity.text = card.insanity.ToString();
+
+        artWork.sprite = card.artWork;
+
+    }
+
+}
